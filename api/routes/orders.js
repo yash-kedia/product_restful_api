e7723ff05757701b714c056eb138c68972971d4c
YASH <yashkedia011@gmail.com>
2021-04-18 13:29:24 +0530

    All done

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -31,41 +31,66 @@ router.get('/',(req,res,next) =>{
 });
 
 router.post('/',(req,res,next) => {
-    const order = new Order({
-        _id: mongoose.Types.ObjectId(),
-        quantity: req.body.quantity,
-        product: req.body.productId 
-    });
-    order.save().then(result => {
-        console.log(result);
-        res.status(201).json({
-            msg: 'Order Created',
-            createdOrder:{
-                _id: result.id,
-                quantity:result.quantity,
-                product:result.product
-
-            },
-            request:{
-                type:'GET',
-                url: 'https://localhost:3000/orders'+result._id
+    Product.findById(req.body.productId).then(
+        product =>{
+            if(!product){
+                return res.status(404).json({
+                    msg:'Product not found'
+                });
             }
-        });}
-    ).catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error:err
-        });
-    });
+
+            const order = new Order({
+                _id: mongoose.Types.ObjectId(),
+                quantity: req.body.quantity,
+                product: req.body.productId 
+            });
+            return order.save();
+        }).then(result => {
+                console.log(result);
+                res.status(201).json({
+                    msg: 'Order Created',
+                    createdOrder:{
+                        _id: result.id,
+                        quantity:result.quantity,
+                        product:result.product
+        
+                    },
+                    request:{
+                        type:'GET',
+                        url: 'https://localhost:3000/orders'+result._id
+                    }
+                });
+            }
+            ).catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error:err
+                });
+            });
    
 });
 
 router.get('/:orderID',(req,res,next) => {
     
-    res.status(200).json({
-        msg: 'order was created',
-        orderId: req.params.orderID
-    });
+   Order.findById(req.params.id)
+   .exec()
+   .then( order => {
+       
+       res.status(200).json({
+           order: order,
+           request: {
+               type:'GET',
+               url: 'http://localhost:3000/orders'
+           }
+
+       });
+   })
+   .catch(err => {
+       console.log(err);
+       res.status(500).json({
+           error:err
+       });
+   });
 });
 
 router.delete('/:orderID',(req,res,next) => {
