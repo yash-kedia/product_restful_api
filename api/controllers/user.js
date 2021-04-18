efb8830b1103550bdc54699cca281c263f1d7d18
Yash Kedia <39669117+yash-kedia@users.noreply.github.com>
2021-04-18 19:31:43 +0530

    Update user.js

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,100 @@
 const mongoose = requirE('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+exports.user_sign_up = (req,res,next) => {
+    User.find({email:req.body.email})
+    .exec()
+    .then(user => {
+        if(user.length>=1){
+            return res.status(409).json({
+                msg: 'Mail exists'
+            });
+        }else{
+            const salt = bcrypt.genSaltSync(10);
+    
+    const user = new User ({
+        _id: new mongoose.Types.ObjectId(),
+        email:req.body.email,
+        password: bcrypt.hashSync(req.body.password,salt)
+    })
+    user.save()
+    .then(
+        result => {
+            console.log(result);
+            return res.status(200).json({
+                msg:'user created'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                error:err
+            });
+        })
 
-exports.user_sign_up = 
+        }
+    });
+    
+    
+
+  
+
+}
+exports.user_log_in = (req,res,next) => {
+    User.findOne({email:req.body.email})
+    .exec()
+    .then( user => {
+        if(user.length<1){
+            return res.status(401).json({
+                msg:'auth failed'
+            })
+        }else{
+            bcrypt.compare(req.body.password,user[0].password),(err,result)=>{
+                if(err){
+                    return res.status(401).json({
+                        msg:'auth failed'
+                    });
+                }
+                if(result){
+                   const token =  jwt.sign({
+                        email: req.user[0].email,
+                        userId : user[0]._id
+                    },
+                    process.env.JWT_KEY,
+                    {
+                        expiresIn : "1h"
+                    }
+
+                    )
+                   return  res.status(200).json({
+                        msg:'auth successfull',
+                        token: token
+                        
+                    });
+                }
+               res.status(401).status.json({
+                   msg:'auth failed'
+               });
+            }
+        }
+    })
+    .catch(err => {
+        return res.status(500).json({
+            error: err
+        });
+    })
+}
+exports.user_delete = (req,res,next) => {
+    User.remove({_id: req.params.id}).exec()
+    .then(res => {
+        return res.status(200).json({
+            msg: 'user deleted'
+        });
+    })
+    .catch(err => {
+        return res.status(500).json({
+            error:err
+        });
+    })
+}
